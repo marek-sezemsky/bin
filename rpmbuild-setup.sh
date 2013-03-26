97bea2fec1e83d8b773c343660f9c7fa6c2ac76b
Marek Sezemsky <marek.sezemsky@gmail.com>
2013-03-26 23:13:46 +0100

    rpmbuild-setup.sh

---
+++ rpmbuild-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Setup RPM build for current non-root user. Install dependencies when
+# required.
+#
+
+set -e
+set -u
+set -x
+
+rpms="rpm rpm-build redhat-rpm-config fedora-packager rpmdevtools"
+rpm -q $rpms || yum install $rpms
+
+if [ $EUID -eq 0 ]; then
+    echo "Must not build RPMs as root!" >&2
+    exit 255
+fi
+
+# rpmdev-setuptree
+mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+
+if [ ! -f ~/.rpmmacros ]; then
+    echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
+fi
