9ed9bea51fd3a5620d4d3d4ce1e1fd05bd16bc55
Marek Sezemsky <marek.sezemsky@deutsche-boerse.com>
2013-05-17 15:38:20 +0200

    Views are listed correctly.
    Fixed double-listing of views because 'prefix* prefix*' was used. Now we show
    either views by argument, by local prefix or by user.

--- cc-view-list.sh
+++ cc-view-list.sh
@@ -1,22 +1,24 @@
 #!/bin/env bash
 #
-# $0
+# $0 [prefix]
 #
-# List view with prefix ${user} or ${LOCAL_VIEW_PREFIX}
+# List view with ${prefix}*, ${user}* or ${LOCAL_VIEW_PREFIX}*
 #
 
 set -u
 set -e
 # set -x
 
-set -f  # disable globbing for lsview cn880*
-
 cleartool=/opt/rational/clearcase/bin/cleartool
 
-user="$(whoami)"
-prefixes="${1:-$user}*"
-if [ -n "${LOCAL_VIEW_PREFIX:-}" ]; then
-    prefixes="$prefixes ${LOCAL_VIEW_PREFIX}*"
+if [ -n "${1:-}" ]; then
+    prefix="${1}"
+elif [ -n "${LOCAL_VIEW_PREFIX:-}" ]; then
+    prefix="${LOCAL_VIEW_PREFIX}"
+else
+    prefix="$(whoami)"
 fi
 
-exec $cleartool lsview $prefixes | sort
+set -f  # disable globbing for lsview cn880*
+
+$cleartool lsview ${prefix}* | sort
